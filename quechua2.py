c2cc62c08731cd67c296a404365a63e8c0c10ac6
Angela2603 <a20180405@pucp.edu.pe>
2024-07-01 19:21:52 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -177,14 +177,14 @@ body {
 }
 
 .caption-custom {
-    color: #DEB887; /* Cambia esto al color que desees */
+    color: #D2B48C; /* Cambia esto al color que desees */
     font-size: 16px; /* Cambia el tamaño de la fuente si es necesario */
     font-family: 'Montserrat', sans-serif;
     text-align: justify; 
     }
 
 .custom-divider {
-    border-top: 2px solid #ccbcaf;  /* Cambia el color y el grosor del divisor */
+    border-top: 2px solid #7f5f42;  /* Cambia el color y el grosor del divisor */
     margin-top: 0px;  /* Ajusta el margen superior */
     margin-bottom: 20px;  /* Ajusta el margen inferior */
 }
