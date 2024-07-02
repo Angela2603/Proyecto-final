94f3be18ae91e8be0cbf24dcac2c7680db25688c
Angela2603 <a20180405@pucp.edu.pe>
2024-07-01 19:05:42 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -177,7 +177,7 @@ body {
 .caption-custom {
     color: #654321; /* Cambia esto al color que desees */
     font-size: 17px; /* Cambia el tamaño de la fuente si es necesario */
-    font-family: 'Oswald', sans-serif;
+    font-family: 'Montserrat', sans-serif;
     text-align: justify; 
     }
 
