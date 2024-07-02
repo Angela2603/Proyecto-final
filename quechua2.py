984b75355c0b2e6560cfe31b1ce77f7eff46ea3d
Angela2603 <a20180405@pucp.edu.pe>
2024-07-01 19:24:52 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -184,7 +184,7 @@ body {
     }
 
 .custom-divider {
-    border-top: 2px solid #DEB887;  /* Cambia el color y el grosor del divisor */
+    border-top: 2px solid ##F5F5DC;  /* Cambia el color y el grosor del divisor */
     margin-top: 0px;  /* Ajusta el margen superior */
     margin-bottom: 20px;  /* Ajusta el margen inferior */
 }
