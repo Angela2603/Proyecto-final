7658dee740a9e79d238c191c2bd3e09a15670ec9
Angela2603 <a20180405@pucp.edu.pe>
2024-07-03 18:02:48 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -336,7 +336,8 @@ st.markdown('<div class="custom-divider"></div>', unsafe_allow_html=True)
 
 #### TRADUCTOR INVERSO
 
-
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
 
 
 
